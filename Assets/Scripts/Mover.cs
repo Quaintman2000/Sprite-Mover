9f28ef8416112d583177617db9049049dd16d98b
q <q>
2020-01-17 18:33:13 -0700

    Added a gameobject, sprite, and speed control

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    //transform variable
+    public Transform tf;
+    public float speed = 0.1f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the transform component
+        tf = GetComponent<Transform>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myVector= new Vector3(2, 4, 12);
+        myVector= myVector.normalized;
+        //moving the sprite upwards by 1 on the y-axis per frame
+        tf.position = tf.position + (myVector * speed);
+    }
+}
